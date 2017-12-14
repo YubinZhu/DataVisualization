1e24b25c087e6b72d1086872e5165f97d04b8911
朱宇斌 <651445686@qq.com>
2017-12-14 15:31:45 +0800

    First commit.

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 朱宇斌 on 2017/12/14
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+/**
+ * Created by 朱宇斌 on 2017/12/14
+ */
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
